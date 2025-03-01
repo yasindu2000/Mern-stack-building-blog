634750d7cdc1065470d01cb47b5c956e8861dc92
yasindu2000 <yasindudahanayake@gmail.com>
2025-03-01 09:42:09 +0530

    update pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,32 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='mb-20'>
+    
+      <h1 className='sm:text-4xl  text-2xl font-bold my-6 text-gray-900 '>About</h1>
+      <p className='mx-auto leading-relaxed text-base mb-4'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. 
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 
+        Duis ut purus a libero bibendum aliquet. Integer nec sapien et metus varius varius. 
+        Curabitur nec justo ac lorem facilisis tincidunt.</p>
+
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. 
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 
+        Duis ut purus a libero bibendum aliquet. Integer nec sapien et metus varius varius. 
+        Curabitur nec justo ac lorem facilisis tincidunt.</p>
+
+         <p className='mx-auto leading-relaxed text-base mb-4'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. 
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere 
+            cubilia curae; Duis ut purus a libero bibendum aliquet. Integer nec sapien 
+            et metus varius varius. Curabitur nec justo ac lorem facilisis tincidunt.
+            Fusce posuere, ligula et congue cursus, nulla eros feugiat magna, non cursus
+            purus enim ut risus. Vivamus sit amet pharetra ipsum. Proin hendrerit eros 
+            ut mauris convallis, id hendrerit arcu volutpat. Pellentesque ac justo ut mi
+            efficitur euismod. Nam interdum, orci nec volutpat pharetra, enim ex porttitor
+            libero, vel tincidunt erat nunc at velit.</p>
+    </div>
   )
 }
 
