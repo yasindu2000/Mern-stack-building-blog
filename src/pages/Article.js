2ae1ff75969f4700336f716814166c2db9485850
yasindu2000 <yasindudahanayake@gmail.com>
2025-03-01 13:33:02 +0530

    update

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -15,14 +15,15 @@ function Article() {
 
 
   return (
-    <div>
+    <>
        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
         {article.title}
        </h1>
        {article.content.map((paragraph, index)=>(
         <p className='mx-auto leading-relaxed text-base mb-4' key={index}>{paragraph}</p>
        ))}
-    </div>
+       <h1 className='sm:text-2xl text-xl font-bold my-4 text-gray-900'>Other Articles</h1>
+    </>
   )
 }
 
