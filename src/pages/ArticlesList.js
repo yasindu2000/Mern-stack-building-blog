f03563b1f59bb9b48d8b5a0de247ad1f7208a3ae
yasindu2000 <yasindudahanayake@gmail.com>
2025-03-01 13:32:48 +0530

    update

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -30,6 +30,15 @@ function ArticlesList() {
                             {article.content[0].substring(0, 110)}...
 
                           </p>
+                          <div className='flex items-center flex-wrap'>
+                            <Link className='text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0'
+                            
+                            to={`/article/${article.name}`}>Learn more..
+                            
+                            
+                            </Link>
+
+                          </div>
                            
                        </div>
                     </div>
