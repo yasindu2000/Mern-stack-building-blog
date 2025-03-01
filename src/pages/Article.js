24092aa68ce318ad25cca357c7368d6fd9528147
yasindu2000 <yasindudahanayake@gmail.com>
2025-03-01 11:47:38 +0530

    update

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import articleContent from './article-content';
 
 function Article() {
 
     const { name } = useParams();
+    const article = articleContent.find((article) =>
+        article.name === name)
 
 
   return (
-    <div className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
-        This is the { name } Articles
+    <div>
+       <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
+        {article.title}
+       </h1>
+       {article.content.map((paragraph, index)=>(
+        <p className='mx-auto leading-relaxed text-base mb-4' key={index}>{paragraph}</p>
+       ))}
     </div>
   )
 }
