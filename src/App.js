b891a123446c9029e0a904edb4a7482ae41ca087
yasindu2000 <yasindudahanayake@gmail.com>
2025-03-01 09:40:11 +0530

    create pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <Router>
+    <div className='max-w-screen-md mx-auto pt-20'>
+      <Routes>
+         <Route path='/' element={ <Home/> }/>
+     </Routes>
     </div>
+    </Router>
   );
 }
 
---
+++ src/pages/ArticlesList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ArticlesList() {
+  return (
+    <div>ArticlesList</div>
+  )
+}
+
+export default ArticlesList
